27e6b89d771baa372bd941a9f856318f35fc9a39
rafin8990 <rafin8990@gmail.com>
2023-10-23 15:53:21 +0600

    middleware added for auth checkup

---
+++ app/Http/Middleware/AuthCheck.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {if(!Session()->has('loginId')){
+        return redirect('login')->with('Fail', 'You have to login first!');
+    }
+        return $next($request);
+    }
+}
