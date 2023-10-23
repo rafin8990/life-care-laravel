991f0acd19a977e5f49f8da1d05b6f4bb2f74001
rafin8990 <rafin8990@gmail.com>
2023-10-23 15:44:34 +0600

    all controller added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
